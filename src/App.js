621f9854d6a706c7bc7990a568f0d6bc8a902c1c
leahobot <leahobot@gmail.com>
2022-10-17 22:53:17 +0100

    deplyment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import {Routes, Route} from "react-router-dom";
-import {Login, Dashboard, Users, UserDetails} from "./pages";
+import {Login, Dashboard, Users, UserDetails, NotFound} from "./pages";
 
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<Login />} />
-			<Route path='/users' element={<Users />} />
-			<Route path='/dashboard' element={<Dashboard />} />
-			<Route path='/user-details/:id' element={<UserDetails />} />
+			<Route path='/lendsqr-fe-test' element={<Login />} />
+			<Route path='/lendsqr-fe-test/users' element={<Users />} />
+			<Route path='/lendsqr-fe-test/dashboard' element={<Dashboard />} />
+			<Route
+				path='/lendsqr-fe-test/user-details/:id'
+				element={<UserDetails />}
+			/>
+			<Route path='*' element={<NotFound />} />
 		</Routes>
 	);
 }
 
 export default App;
-
-// <Route path='*' element={<NotFound />} />
