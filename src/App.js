4cd129e49db5d03552a5e6cdf1e365c9ad89bab8
leahobot <leahobot@gmail.com>
2022-10-17 20:27:11 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {Login, Dashboard, Users, UserDetails} from "./pages";
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<Login />} />
-			<Route path='/login' element={<Navigate to='/' />} />
 			<Route path='/users' element={<Users />} />
 			<Route path='/dashboard' element={<Dashboard />} />
 			<Route path='/user-details/:id' element={<UserDetails />} />
